1fdae354c78f19db9a16a6445e7b6a065e4a2461
Tiina Matala <t7mati01@students.oamk.fi>
2020-03-02 08:49:02 +0200

    some changes in app.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+//const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testing</Text>
     </View>
   );
 }
