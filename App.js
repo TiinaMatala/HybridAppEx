098b3e6c5140757bd906bccb27f8a2c88e6cf564
Tiina Matala <t7mati01@students.oamk.fi>
2020-03-10 20:11:33 +0200

    changes in App.js

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-//import React, { Component } from 'react'
 import { StyleSheet, Text, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import ItemsMain from './components/ItemsMain'
@@ -8,20 +7,6 @@ import ItemsMain from './components/ItemsMain'
 //const Stack = createStackNavigator();
 
 export default function App() {
-
-  const items = [{
-    title: 'Rubber boots',
-    description: 'Yellow, size 36',
-    category: 'shoes and clothes',
-    location: 'Oulu',
-    askingPrice: '5',
-    dateOfPosting: '08-03-2020',
-    deliveryTypeShipping: true,
-    deliveryTypePickup: false,
-    sellerInfoName: 'Nina',
-    sellerInfoPhone: '0401234567',
-    images: 'www.shoes.com'
-  }]
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
